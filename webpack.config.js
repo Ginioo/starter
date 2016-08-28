f4a621e5adb3fa8008a6607c05ead016d0955ffd
Gino Wu <nikeorz+github@gmail.com>
2016-08-28 08:22:12 +0000

    init, set up webpack with babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+        },
+        output: {
+            comments: false,
+        },
+    }),
+  ]
+};
