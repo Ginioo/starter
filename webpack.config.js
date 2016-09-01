355eb3c7c1a10c1fbf4f30ece9706b670fd3d837
Gino Wu <nikeorz+github@gmail.com>
2016-09-01 02:03:03 +0000

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,58 @@
-var webpack = require("webpack");
-var path = require("path");
+const path = require("path");
+const merge = require("webpack-merge");
+const validate = require("webpack-validator");
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-var publicPath = '/starter/';
+const PATHS = {
+  app: path.join(__dirname, "src", "index"),
+  build: path.join(__dirname, "build"),
+  baseHref: "/starter/"
+};
 
-module.exports = {
-  entry: [
-    './src/index'
-  ],
-  output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: publicPath,
-    filename: 'bundle.js'
+const common = {
+  // Entry accepts a path or an object of entries.
+  // We"ll be using the latter form given it"s
+  // convenient with more complex configurations.
+  entry: {
+    app: PATHS.app
   },
-  module: {
-    loaders: [
-      {
-        test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
+  output: {
+    path: PATHS.build,
+    filename: "[name].js",
+    publicPath: PATHS.baseHref
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Starter',
-      baseHref: publicPath,
-      filename: 'index.html',
-      template: 'templates/index.html',
+      title: "Starter",
+      baseHref: PATHS.baseHref,
+      filename: "index.html",
+      template: "templates/index.html",
       minify: false,
       hash: true,
       cache: true
     })
-  ]
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
 };
+
+var config;
+
+// Detect how npm is run and branch based on that
+switch(process.env.npm_lifecycle_event) {
+  case "build":
+    config = merge(common, {});
+    break;
+  default:
+    config = merge(common, {});
+}
+
+module.exports = validate(config);
