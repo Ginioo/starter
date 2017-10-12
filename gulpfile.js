4a6f45e3a6a1285b16454a9f0b3226476d7d67f5
Gino <nikeorz@gmail.com>
2017-10-13 06:45:43 +0800

    fix config file with eslint

--- gulpfile.js
+++ gulpfile.js
@@ -5,51 +5,52 @@ const webpack = require('webpack');
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
+const ROOT_PATH = path.dirname();
 
 gulp.task('clean', (callback) => {
-  del([
-    'build',
-  ]);
-  callback();
+    del([
+        'build',
+    ]);
+    callback();
 });
 
 gulp.task('webpack', (callback) => {
-  const productionConfig = require('./webpack.config.js')('production');
-  // run webpack
-  webpack(productionConfig, function(err, stats) {
-    if(err) throw new gutil.PluginError('webpack', err);
-    gutil.log('[webpack]', stats.toString({
-      // output options
-      colors: true
-    }));
-    callback();
-  });
+    const productionConfig = require('./webpack.config.js')('production');
+    // run webpack
+    webpack(productionConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError('webpack', err);
+        gutil.log('[webpack]', stats.toString({
+            // output options
+            colors: true
+        }));
+        callback();
+    });
 });
 
 gulp.task('build', ['webpack'], (callback) => {
-  const productionConfig = require('./webpack.config.js')('production');
-  const htaccessTemplate = fs.readFileSync(path.join(__dirname, 'templates', '.htaccess')).toString();
-  const htaccess = _.template(htaccessTemplate)({REWRITE_BASE: productionConfig.output.publicPath});
-  fs.writeFile(path.join(__dirname, 'build', '.htaccess'), htaccess);
+    const productionConfig = require('./webpack.config.js')('production');
+    const htaccessTemplate = fs.readFileSync(path.join(ROOT_PATH, 'templates', '.htaccess')).toString();
+    const htaccess = _.template(htaccessTemplate)({REWRITE_BASE: productionConfig.output.publicPath});
+    fs.writeFile(path.join(ROOT_PATH, 'build', '.htaccess'), htaccess);
 
-  gutil.log('[build]', 'successfully');
-  callback();
+    gutil.log('[build]', 'successfully');
+    callback();
 });
 
 const WebpackDevServer = require('webpack-dev-server');
 gulp.task('dev', (callback) => {
-  const developmentConfig = require('./webpack.config.js')('development');
-  // Start a webpack-dev-server
-  new WebpackDevServer(webpack(developmentConfig), {
-    publicPath: developmentConfig.output.publicPath,
-    stats: {colors: true}
-  })
-  .listen(developmentConfig.devServer.port, developmentConfig.devServer.host, (err) => {
-    if (err) throw new gutil.PluginError('webpack-dev-server', err);
-    gutil.log(
-      '[webpack-dev-server]',
-      'http://' + developmentConfig.devServer.host + ':' + developmentConfig.devServer.port + '/webpack-dev-server' + (developmentConfig.output.publicPath ? developmentConfig.output.publicPath : '/') + 'index.html'
-    );
-  });
-  callback();
+    const developmentConfig = require('./webpack.config.js')('development');
+    // Start a webpack-dev-server
+    new WebpackDevServer(webpack(developmentConfig), {
+        publicPath: developmentConfig.output.publicPath,
+        stats: {colors: true}
+    })
+        .listen(developmentConfig.devServer.port, developmentConfig.devServer.host, (err) => {
+            if (err) throw new gutil.PluginError('webpack-dev-server', err);
+            gutil.log(
+                '[webpack-dev-server]',
+                'http://' + developmentConfig.devServer.host + ':' + developmentConfig.devServer.port + '/webpack-dev-server' + (developmentConfig.output.publicPath ? developmentConfig.output.publicPath : '/') + 'index.html'
+            );
+        });
+    callback();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,101 +2,101 @@ const path = require('path');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const parts = require('./libs/parts');
-
+const ROOT_PATH = path.dirname();
 const PATHS = {
-  app: path.join(__dirname, 'src', 'index'),
-  appStyle: path.join(__dirname, 'src', 'style'),
-  appImage: path.join(__dirname, 'src', 'images'),
-  build: path.join(__dirname, 'build'),
-  baseHref: '' // baseHref: '/starter/'
+    app: path.join(ROOT_PATH, 'src', 'index'),
+    appStyle: path.join(ROOT_PATH, 'src', 'style'),
+    appImage: path.join(ROOT_PATH, 'src', 'images'),
+    build: path.join(ROOT_PATH, 'build'),
+    baseHref: '' // baseHref: '/starter/'
 };
 
 const common = {
-  // Entry accepts a path or an object of entries.
-  // We'll be using the latter form given it's
-  // convenient with more complex configurations.
-  entry: {
-    app: PATHS.app
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js',
-    publicPath: PATHS.baseHref
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Hello!',
-      baseHref: PATHS.baseHref,
-      filename: 'index.html',
-      template: 'templates/index.html',
-      minify: {
-        removeAttributeQuotes: true,
-        collapseWhitespace: true,
-        html5: true,
-        minifyCSS: true,
-        removeComments: true,
-        removeEmptyAttributes: true
-      },
-      hash: true,
-      cache: true
-    })
-  ]
+    // Entry accepts a path or an object of entries.
+    // We'll be using the latter form given it's
+    // convenient with more complex configurations.
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js',
+        publicPath: PATHS.baseHref
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Hello!',
+            baseHref: PATHS.baseHref,
+            filename: 'index.html',
+            template: 'templates/index.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                html5: true,
+                minifyCSS: true,
+                removeComments: true,
+                removeEmptyAttributes: true
+            },
+            hash: true,
+            cache: true
+        })
+    ]
 };
 
 const productionConfig = () => {
-  return merge(
-    common,
-    {
-      devtool: 'source-map',
-      output: {
-        path: PATHS.build,
-        filename: '[name].[chunkhash].js',
-        // This is used for require.ensure. The setup
-        // will work without but this is useful to set.
-        chunkFilename: '[chunkhash].js'
-      }
-    },
-    parts.minify(),
-    parts.clean(PATHS.build),
-    parts.setupImages(PATHS.appImage),
-    // parts.setEnvironmentVariable('process.env.NODE_ENV', 'production'),
-    parts.extractBundle({
-      name: 'vendor',
-      entries: ['react', 'react-dom']
-    }),
-    parts.setupJSON(),
-    parts.extractCSS(),
-    parts.setupCSS(PATHS.appStyle),
-    parts.setupBabel()
-  );
+    return merge(
+        common,
+        {
+            devtool: 'source-map',
+            output: {
+                path: PATHS.build,
+                filename: '[name].[chunkhash].js',
+                // This is used for require.ensure. The setup
+                // will work without but this is useful to set.
+                chunkFilename: '[chunkhash].js'
+            }
+        },
+        parts.minify(),
+        parts.clean(PATHS.build),
+        parts.setupImages(PATHS.appImage),
+        // parts.setEnvironmentVariable('process.env.NODE_ENV', 'production'),
+        parts.extractBundle({
+            name: 'vendor',
+            entries: ['react', 'react-dom']
+        }),
+        parts.setupJSON(),
+        parts.extractCSS(),
+        parts.setupCSS(PATHS.appStyle),
+        parts.setupBabel()
+    );
 };
 
 const developmentConfig = () => {
-  return merge(
-    common,
-    {
-      devtool: 'eval-source-map'
-    },
-    parts.setupImages(PATHS.appImage),
-    parts.extractBundle({
-      name: 'vendor',
-      entries: ['react', 'react-dom']
-    }),
-    parts.setupJSON(),
-    parts.extractCSS(),
-    parts.setupCSS(PATHS.appStyle),
-    parts.setupBabel(),
-    parts.devServer({
-      // Customize host/port here if needed
-      // host: 'localhost',
-      port: 8080
-    })
-  );
+    return merge(
+        common,
+        {
+            devtool: 'eval-source-map'
+        },
+        parts.setupImages(PATHS.appImage),
+        parts.extractBundle({
+            name: 'vendor',
+            entries: ['react', 'react-dom']
+        }),
+        parts.setupJSON(),
+        parts.extractCSS(),
+        parts.setupCSS(PATHS.appStyle),
+        parts.setupBabel(),
+        parts.devServer({
+            // Customize host/port here if needed
+            // host: 'localhost',
+            port: 8080
+        })
+    );
 };
 
 module.exports = (env) => {
-  console.log('current env:', env);
+    // console.log('current env:', env);
 
-  if (env === 'production') return productionConfig();
-  return developmentConfig();
+    if (env === 'production') return productionConfig();
+    return developmentConfig();
 };
